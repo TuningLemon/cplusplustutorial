1d65f7c716ad81220e3745b5d4219220317af39a
TuningLemon <veststar1@gmail.com>
2021-02-09 19:53:42 +0900

    main function

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int answer = 0;
+    cout << "Enter Your Favorite Number Between 1 and 100";
+    cin >> answer;
+    cout << "Amazing!! That's my favorite number too!\nNo really!!,"
+         << answer << "is my favorite number" << endl;
+    return 0;
+}
